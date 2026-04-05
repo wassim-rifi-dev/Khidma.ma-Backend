e39aca5e7e8a05470e949184c47e6a970854aed8
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-04-05 21:30:41 +0100

    feat: ajoute fillable dans profissionals model

--- app/Models/professional.php
+++ app/Models/professional.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class professional extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'category',
+        'city'
+    ];
 }
