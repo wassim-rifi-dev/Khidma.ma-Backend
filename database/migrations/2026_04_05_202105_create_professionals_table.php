e08a3479a3a61f2a5162d23ca28049d9084a65ac
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-04-05 21:29:01 +0100

    feat: ajoute column dans professionals tableau

--- database/migrations/2026_04_05_202105_create_professionals_table.php
+++ database/migrations/2026_04_05_202105_create_professionals_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('professionals', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->enum('category' , []);
+            $table->string('city');
             $table->timestamps();
         });
     }
