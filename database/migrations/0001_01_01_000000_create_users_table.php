72e5e3d5e69b1784d566284b885e73758c5b895f
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-04-05 21:11:31 +0100

    feat: ajoute des column dans le tableau de user et suprimer des column

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,9 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone')->unique();
+            $table->enum('role' , ['client' , 'professional' , 'admin']);
+            $table->boolean('is_active')->default(true);
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
 
