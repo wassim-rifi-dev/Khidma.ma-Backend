3d0044e76c9a19d6a6c3dec9c7e8e2e3a92090d1
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-04-06 17:42:23 +0100

    feat: ajoute register route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +9,6 @@ Route::get('/test', function () {
         'success' => true,
     ]);
 });
+
+// Auth
+Route::post('register' , [RegisterController::class , 'register']); // Register
