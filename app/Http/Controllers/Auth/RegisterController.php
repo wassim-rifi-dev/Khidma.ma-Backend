0e3ea8665dabdce9ea947e5d9df9262c7ae909c6
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-04-06 17:50:19 +0100

    feat: ajoute auth token dans le register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -32,49 +32,15 @@ class RegisterController extends Controller
         ];
 
         $user = $authServices->createNewUser($data);
+        $token = $user->createToken('auth-token')->plainTextToken;
 
         return response()->json([
             "success" => true,
             "data" => [
                 "user" => $user,
+                "token" => $token
             ],
             "message" => "Bonjour $user->name, votre compt creer en succe"
         ], 201);
     }
-
-    // public function register(RegisterRequest $registerRequest , AuthServices $authServices) {
-    //         $user = $authServices->existeEmail($registerRequest->email);
-
-    //         if ($user) {
-    //             return response()->json(
-    //                 [
-    //                     "success" => false,
-    //                     "data" => [],
-    //                     "message" => "Il existe un utilisateur avec cet email"
-    //                 ],
-    //                 401
-    //             );
-    //         }
-
-    //         $data = [
-    //             'name' => $registerRequest->name,
-    //             'email' => $registerRequest->email,
-    //             'password' => Hash::make($registerRequest->password)
-    //         ];
-    //         $newUser = $authServices->createAccount($data);
-
-    //         $token = $newUser->createToken('auth-token')->plainTextToken;
-
-    //         return response()->json(
-    //             [
-    //                     "success" => true,
-    //                     "data" => [
-    //                         'user' => $newUser,
-    //                         'token' => $token
-    //                     ],
-    //                     "message" => "Bonjour $newUser->name, votre compt creer en succe"
-    //                 ] ,
-    //                 201
-    //         );
-    //     }
 }
