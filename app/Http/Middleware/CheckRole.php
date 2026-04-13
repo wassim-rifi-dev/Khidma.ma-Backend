8b5d3ebf4a4a19b2d108a8c512a553d3cecb1d6a
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-04-13 10:47:16 +0100

    feat: creation de middleware de CheckRole

---
+++ app/Http/Middleware/CheckRole.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next, ...$roles)
+    {
+        if (!$request->user()) {
+            return response()->json(['message' => 'Unauthenticated'], 401);
+        }
+
+        if (!in_array($request->user()->role, $roles)) {
+            return response()->json(['message' => 'Unauthorized'], 403);
+        }
+
+        return $next($request);
+    }
+}
