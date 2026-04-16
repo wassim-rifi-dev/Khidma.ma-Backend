f07579a794d2ce41ba71f35423423ee5fa4f53e2
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-04-16 15:09:22 +0100

    feat: ajoute relation dans categories model avec service

--- app/Models/Categories.php
+++ app/Models/Categories.php
@@ -3,10 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Categories extends Model
 {
     protected $fillable = [
         'name'
     ];
+
+    public function services(): HasMany
+    {
+        return $this->hasMany(Service::class, 'categorie_id');
+    }
 }
