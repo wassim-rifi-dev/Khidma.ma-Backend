b97eabe469111417f82d7d9b269c68dec96d859a
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-04-16 16:42:03 +0100

    feat: ajoute fonction de store dans ServiceController

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\Service\StoreServiceRequest;
+use App\Services\ProfessionalServices;
+use App\Services\ServiceServices;
 
 class ServiceController extends Controller
 {
-    
+    public function store(StoreServiceRequest $request, ServiceServices $serviceServices, ProfessionalServices $professionalServices) {
+        $professional = $professionalServices->getProfessionalInfo((int) $request->user()->id);
+
+        if (!$professional) {
+            return response()->json([
+                'success' => false,
+                'data' => [],
+                'message' => 'Professional profile not found'
+            ], 404);
+        }
+
+        $data = array_merge($request->validated(), [
+            'professional_id' => $professional->id,
+        ]);
+
+        $service = $serviceServices->createServices($data);
+
+        return response()->json([
+            'success' => true,
+            'data' => [
+                'service' => $service,
+            ],
+            'message' => 'Service created successfully'
+        ], 201);
+    }
 }
