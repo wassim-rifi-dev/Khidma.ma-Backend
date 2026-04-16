7d70d20849c570bae6e938039bab9672be140dda
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-04-16 15:01:48 +0100

    feat: ajoute les relation dans le model de services

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Service extends Model
 {
@@ -15,4 +17,14 @@ class Service extends Model
         'price_min',
         'price_max',
     ];
+
+    public function professional()
+    {
+        return $this->belongsTo(Professional::class, 'professional_id');
+    }
+
+    public function categorie()
+    {
+        return $this->belongsTo(Categories::class, 'categorie_id');
+    }
 }
