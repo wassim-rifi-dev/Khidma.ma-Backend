8d31a550b6d7582037baf44763c86397064dbab4
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-04-17 15:03:41 +0100

    feat: creation du migration , model , et controller de table du messages et ajoute c'est column

---
+++ database/migrations/2026_04_17_135705_create_messages_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('sender_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('chat_id')->constrained('chats')->onDelete('cascade');
+            $table->text('message');
+            $table->enum('message_type' , ['text' , 'image' , 'video' , 'file']);
+            $table->string('media_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('messages');
+    }
+};
