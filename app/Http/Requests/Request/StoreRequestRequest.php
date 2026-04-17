1f96e8027c76ff3cc3df7cfa7f98f126ac3ab4a1
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-04-17 09:21:57 +0100

    feat: ajoute StoreRequestRequest

---
+++ app/Http/Requests/Request/StoreRequestRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Request;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequestRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'service_id' => 'required|integer|exists:services,id',
+            'message' => 'required|string|max:255',
+            'status' => 'nullable|string|in:Nouveau,En_Cour,Terminer',
+        ];
+    }
+}
