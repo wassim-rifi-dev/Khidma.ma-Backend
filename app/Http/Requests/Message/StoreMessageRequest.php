620a1dcc5e2abc9a4215661f81a30559aef9feb2
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-04-17 15:14:46 +0100

    feat: ajoute StoreMEssageRequest

---
+++ app/Http/Requests/Message/StoreMessageRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Message;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreMessageRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'message' => 'required|string',
+            'message_type' => 'required|string|in:text,image,video,file',
+            'media_url' => 'nullable|string|max:255',
+        ];
+    }
+}
