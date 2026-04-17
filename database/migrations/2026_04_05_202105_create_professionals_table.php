cd2feea700b25048bb42b22f6c0780d555e564a4
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-04-17 15:06:23 +0100

    feat: modifier les column des tableau

--- database/migrations/2026_04_05_202105_create_professionals_table.php
+++ database/migrations/2026_04_05_202105_create_professionals_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('professionals', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('user_id')->unique()->constrained('users')->onDelete('cascade');
             $table->foreignId('categorie_id')->constrained('categories')->onDelete('cascade');
             $table->string('city');
             $table->float('rating')->default(0.00);
--- database/migrations/2026_04_17_092906_create_reviews_table.php
+++ database/migrations/2026_04_17_092906_create_reviews_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('order_id')->constrained('requests')->onDelete('cascade');
+            $table->foreignId('order_id')->unique()->constrained('requests')->onDelete('cascade');
             $table->foreignId('client_id')->constrained('users')->onDelete('cascade');
             $table->float('rating')->default(0.00);
             $table->text('comment')->nullable();
