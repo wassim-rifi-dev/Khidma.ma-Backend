e84997e8b38be455ab854b523ae047f3b3daf07d
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-04-17 11:02:04 +0100

    feat: ajoute les column dans tableau de reviews

--- database/migrations/2026_04_17_092906_create_reviews_table.php
+++ database/migrations/2026_04_17_092906_create_reviews_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('order_id')->constrained('requests')->onDelete('cascade');
+            $table->foreignId('client_id')->constrained('users')->onDelete('cascade');
+            $table->float('rating')->default(0.00);
+            $table->text('comment')->nullable();
             $table->timestamps();
         });
     }
